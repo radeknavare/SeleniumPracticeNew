07f22f943bd680b1af853a4a863afb2b92dec905
radeknavare <radeknavare@gmail.com>
2019-05-30 15:05:42 +0530

    Add files via upload

---
+++ SeleniumPractice/src/test/java/com/seleniumpractice/TestNg.java
@@ -0,0 +1,17 @@
+package com.seleniumpractice;
+
+import org.testng.annotations.Test;
+
+public class TestNg {
+
+	@Test
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void World()
+	{
+		System.out.println("World");
+	}
+}
